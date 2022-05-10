59ce0ab6bb8fbd329e5e9362b2f27fab4fea9d90
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-05-10 16:14:48 -0500

    setting up models and routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,115 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User, Comment, Vote } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//get all comments
+router.get('/', (req, res) => {
+    Comment.findAll({
+        attributes: [
+            'id',
+            'comment_text'
+        ],
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'post_url', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/:id', (req, res) => {
+    Comment.findOne({
+        attributes: [
+            'id',
+            'comment_text'
+        ],
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'post_url', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.commet_text
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+})
+
+router.put('/:id', (req, res) => {
+    Comment.update(
+        {
+            comment_text: req.nody.comment_text
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No comment found with this id' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No comment found with this id' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
