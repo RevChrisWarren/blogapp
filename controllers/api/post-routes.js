d622d158aaee7b304e203210cdbf8c743213e773
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-05-17 16:45:07 -0500

    fixed delete error

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -124,22 +124,27 @@ router.put('/:id', withAuth, (req, res) => {
 
 router.delete('/:id', withAuth, (req, res) => {
     console.log('id', req.params.id);
-    Post.destroy({
+    Comment.destroy({
         where: {
-            id: req.params.id
+            post_id: req.params.id
         }
-    })
-        .then(dbPostData => {
-            if (!dbPostData) {
-                res.status(404).json({ message: 'No post found with this id' });
-                return;
+    }).then(() => {
+        Post.destroy({
+            where: {
+                id: req.params.id
             }
-            res.json(dbPostData);
         })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+            .then(dbPostData => {
+                if (!dbPostData) {
+                    res.status(404).json({ message: 'No post found with this id' });
+                    return;
+                }
+                res.json(dbPostData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    });
 });
-
 module.exports = router;
